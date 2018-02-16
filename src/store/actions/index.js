3c36d0e144b06204a601682b1af1eabcb3acddf9
Alexandre Gachet <alex.gachet@gmail>
2018-02-16 17:42:28 +0100

    Use redux-saga for burgerBuilder and order

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,12 +1,20 @@
 export {
     addIngredient,
     removeIngredient,
-    initIngredients
+    initIngredients,
+    setIngredients,
+    fetchIngredientsFailed
 } from './burgerBuilder'
 export {
     purchaseBurger,
     purchaseInit,
-    fetchOrders
+    fetchOrders,
+    fetchOrdersStart,
+    fetchOrdersSuccess,
+    fecthOrdersFail,
+    purchaseBurgerStart,
+    purchaseBurgerSuccess,
+    purchaseBurgerFail
 } from './order'
 export {
     auth,
